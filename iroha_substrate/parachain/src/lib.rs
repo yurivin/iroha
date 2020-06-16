e9328bbddbea8c7db015ff2278efc3be3bd56251
iceseer <iceseer@gmail.com>
2020-06-16 19:11:47 +0300

    1

--- iroha_substrate/parachain/src/lib.rs
+++ iroha_substrate/parachain/src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright 2017-2020 Parity Technologies (UK) Ltd.
+// Copyright 2020 Parity Technologies (UK) Ltd.
 // This file is part of Polkadot.
 
 // Polkadot is free software: you can redistribute it and/or modify
@@ -14,40 +14,4 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
-//! Defines primitive types for creating or validating a parachain.
-//!
-//! When compiled with standard library support, this crate exports a `wasm`
-//! module that can be used to validate parachain WASM.
-//!
-//! ## Parachain WASM
-//!
-//! Polkadot parachain WASM is in the form of a module which imports a memory
-//! instance and exports a function `validate_block`.
-//!
-//! `validate` accepts as input two `i32` values, representing a pointer/length pair
-//! respectively, that encodes [`ValidationParams`].
-//!
-//! `validate` returns an `u64` which is a pointer to an `u8` array and its length.
-//! The data in the array is expected to be a SCALE encoded [`ValidationResult`].
-//!
-//! ASCII-diagram demonstrating the return data format:
-//!
-//! ```ignore
-//! [pointer][length]
-//!   32bit   32bit
-//!         ^~~ returned pointer & length
-//! ```
-//!
-//! The wasm-api (enabled only when `std` feature is not enabled and `wasm-api` feature is enabled)
-//! provides utilities for setting up a parachain WASM module in Rust.
-
-#![cfg_attr(not(feature = "std"), no_std)]
-
-#[cfg(feature = "std")]
-pub mod wasm_executor;
-pub mod primitives;
-
-mod wasm_api;
-
-#[cfg(all(not(feature = "std"), feature = "wasm-api"))]
-pub use wasm_api::*;
+//! Stub - the fundamental logic of this crate is the integration tests.
